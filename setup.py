1a9b8bd5d2f07076cf8f84ae5310c0f64b6bd8c3
Jesué Junior <jesuejunior@users.noreply.github.com>
2023-03-29 14:22:40 -0400

    Updating deps versions

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(name='tap-facebook',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_facebook'],
       install_requires=[
-          'attrs==17.3.0',
-          'backoff==1.8.0',
-          'pendulum==1.2.0',
+          'attrs>=17.3.0',
+          'backoff>=1.8.0',
+          'pendulum>=1.2.0',
           'facebook_business==15.0.0',
-          'requests==2.20.0',
-          'singer-python==5.10.0',
+          'requests>=2.20.0',
+          'singer-python>=5.10.0',
       ],
       extras_require={
           'dev': [
